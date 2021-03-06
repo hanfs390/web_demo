05d5f612722bfe135163bab6729b517a752ba8cb
hanfushun <hanfs390@foxmail.com>
2021-03-06 17:49:09 +0800

    Add script: compress the front image
    Signed-off-by: hanfushun <hanfs390@foxmail.com>

---
+++ picture_front.py
@@ -0,0 +1,93 @@
+from PIL import Image
+import os
+import sys
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+import shutil
+
+def get_size(file):
+    # 获取文件大小:KB
+    size = os.path.getsize(file)
+    return size / 1024
+
+def get_outfile(infile, outfile):
+    if outfile:
+        return outfile
+    dir, suffix = os.path.splitext(infile)
+    outfile = '{}-out{}'.format(dir, suffix)
+    return outfile
+
+def get_outfile_front(infile, outfile):
+    if outfile:
+        return outfile
+    dir, suffix = os.path.splitext(infile)
+    outfile = '{}-front{}'.format(dir, suffix)
+    return outfile
+
+def compress_image(infile, outfile='', mb=100, step=10, quality=80):
+    """不改变图片尺寸压缩到指定大小
+    :param infile: 压缩源文件
+    :param outfile: 压缩文件保存地址
+    :param mb: 压缩目标，KB
+    :param step: 每次调整的压缩比率
+    :param quality: 初始压缩比率
+    :return: 压缩文件地址，压缩文件大小
+    """
+    print('Start compress ', infile)
+    outfile = get_outfile_front(infile, outfile)
+    o_size = get_size(infile)
+    if o_size <= mb:
+        im = Image.open(infile)
+        im.save(outfile, quality=quality) 
+        return infile
+    while o_size > mb:
+        im = Image.open(infile)
+        im.save(outfile, quality=quality)
+        if quality - step < 0:
+            break
+        quality -= step
+        o_size = get_size(outfile)
+    return outfile, get_size(outfile)
+
+#有拉伸效果
+def resize_image(infile, outfile='', x_s=480):
+    """修改图片尺寸
+    :param infile: 图片源文件
+    :param outfile: 重设尺寸文件保存地址
+    :param x_s: 设置的宽度
+    :return:
+    """
+    im = Image.open(infile)
+    x, y = im.size
+    #y_s = int(y * x_s / x)
+    y_s = 640
+    out = im.resize((x_s, y_s), Image.ANTIALIAS)
+    outfile = get_outfile(infile, outfile)
+    out.save(outfile)
+
+
+if __name__ == '__main__':
+    path = sys.argv[1]
+    print(path)
+    lsdir = os.listdir(path)
+    for i in lsdir:
+        if os.path.isdir(os.path.join(path, i)):
+             path1 = os.path.join(path, i)
+             lsdir1 = os.listdir(path1)
+             for j in lsdir1:
+                 if os.path.isdir(os.path.join(path1, j)):
+                     path2 = os.path.join(path1, j)
+                     lsdir2 = os.listdir(path2)
+                     for m in lsdir2:
+                         if os.path.isdir(os.path.join(path2, m)):
+                             if os.path.exists(os.path.join(path2, m, '1.jpg')):
+                                 if (os.path.getsize(os.path.join(path2, m, '1.jpg')) < 7000):# < 6.2k file will be del
+                                     shutil.rmtree(os.path.join(path2, m))
+                                     continue
+                                 if not os.path.exists(os.path.join(path2, m, '1-front.jpg')):
+                                     compress_image(os.path.join(path2, m, '1.jpg'))
+                             else:
+                                 print(os.path.join(path2, m))
+                                 shutil.rmtree(os.path.join(path2, m))
+                                 
+#    compress_image(r'./test/1.jpg')
